1dbf7c88baffb9de78574c47b0167097052b7f8f
Aniketh <aaatipamula@gmail.com>
2022-10-23 00:30:43 -0500

    changed sourcing bashrc to loading nvm directly into shell

--- setupscripts/init_setup.sh
+++ setupscripts/init_setup.sh
@@ -21,8 +21,10 @@ sudo apt install htop ssh.server vim tmux docker rsync python3 neofetch neovim
 # Install nvm for node version control etc.
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-# Reload for nvm
-source ~/.bashrc
+# Load nvm without restarting shell/terminal
+export NVM_DIR="/home/runner/.nvm"
+[ -s "$NVM_DIR/nvm.sh"  ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion"  ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Install node 14.10.0
 nvm install 14.10.0
