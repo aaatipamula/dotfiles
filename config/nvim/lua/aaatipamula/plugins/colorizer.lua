f978fbbdb20e476a57a6dee8fd82e0c6eb2c5959
Aniketh A. <aaatipamula@gmail.com>
2025-11-11 09:18:31 -0600

    use a different colorizer

--- config/nvim/lua/aaatipamula/plugins/colorizer.lua
+++ config/nvim/lua/aaatipamula/plugins/colorizer.lua
@@ -1,10 +1,26 @@
 return {
-  "norcalli/nvim-colorizer.lua",
-  config = function ()
-    require 'colorizer'.setup {
-      '*'; -- Highlight all files, but customize some others.
-      css = { hsl_fn = true; }; -- Enable parsing all colors functions in css.
-      html = { names = false; } -- Disable parsing "names" like Blue or Gray
-    }
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    config = function ()
+      require("colorizer").setup({
+        user_default_options = {
+          names = false, -- "Name" codes like Blue or red.  Added from `vim.api.nvim_get_color_map()`
+          css = true, -- Enable all CSS *features*:
+          -- Tailwind colors.  boolean|'normal'|'lsp'|'both'.  True sets to 'normal'
+          tailwind = true, -- Enable tailwind colors
+          tailwind_opts = { -- Options for highlighting tailwind names
+            update_names = false, -- When using tailwind = 'both', update tailwind names from LSP results.  See tailwind section
+          },
+          xterm = true, -- Enable xterm 256-color codes (#xNN, \e[38;5;NNNm)
+          -- Highlighting mode.  'background'|'foreground'|'virtualtext'
+          mode = "virtualtext", -- Set the display mode
+          -- Virtualtext character to use
+          virtualtext = "",
+          -- Display virtualtext inline with color.  boolean|'before'|'after'.  True sets to 'after'
+          virtualtext_inline = "before",
+          -- Virtualtext highlight mode: 'background'|'foreground'
+          virtualtext_mode = "foreground",
+        },
+      })
   end
 }
