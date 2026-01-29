88d0e917ee4eccdee575ff0c205d2e270e4663f6
Aniketh A. <aaatipamula@gmail.com>
2026-01-29 14:15:34 -0600

    chore: 🧹 make imgpick a script, update nvim.telescope to ignore node_modules

--- config/nvim/lua/aaatipamula/plugins/telescope.lua
+++ config/nvim/lua/aaatipamula/plugins/telescope.lua
@@ -1,27 +1,33 @@
 -- Fuzzy finding, assorted pickers
 
 return {
-    'nvim-telescope/telescope.nvim',
+  'nvim-telescope/telescope.nvim',
 
-    lazy = false,
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('telescope').setup({})
+  lazy = false,
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('telescope').setup({
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules"
+        }
+      }
+    })
 
-      local builtin = require('telescope.builtin')
+    local builtin = require('telescope.builtin')
 
-      -- Finding files
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-      vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
-      vim.keymap.set('n', '<leader>cs', builtin.colorscheme, {})
-      vim.keymap.set('n', '<leader>tr', builtin.treesitter, {})
-      vim.keymap.set('n', '<leader>jl', builtin.jumplist, {})
+    -- Finding files
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+    vim.keymap.set('n', '<leader>cs', builtin.colorscheme, {})
+    vim.keymap.set('n', '<leader>tr', builtin.treesitter, {})
+    vim.keymap.set('n', '<leader>jl', builtin.jumplist, {})
 
-      -- Spell Suggest
-      vim.keymap.set('n', '<leader>z', builtin.spell_suggest, {})
-    end
+    -- Spell Suggest
+    vim.keymap.set('n', '<leader>z', builtin.spell_suggest, {})
+  end
 }
 
